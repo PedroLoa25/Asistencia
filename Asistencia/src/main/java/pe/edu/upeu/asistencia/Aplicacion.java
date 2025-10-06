838303245dda73bf363597e0f662cad71d7476d7
Pedro Eduardo Loayza huaman <pfloayza1704@gmail.com>
2025-10-05 19:46:55 -0500

    test

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplicacion.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplicacion.java
@@ -2,7 +2,7 @@ package pe.edu.upeu.asistencia;
 
 public class Aplicacion {
     public static void main(String[] args) {
-        System.out.println("Iniciando Aplicacion DMP");
+        System.out.println("Iniciando Aplicacion LOA");
         AsistenciaApplication.main(args);
     }
 }
